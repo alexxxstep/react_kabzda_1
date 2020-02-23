8b8641af89548ba3e1f547902eef79bdaa788011
alexxxstep <astep2004@gmail.com>
2020-02-23 22:06:09 +0200

    add css and containers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <ul><li>Alex</li>
-        <li>DDD</li>
-        <li>SSS</li>
-        <li>WWW</li>
-        <li>Alex</li>
-        <li>react</li>
-      </ul>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://react.semantic-ui.com/logo.png"></img>
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+
+      <div className="content">
+        <div>
+          <img src="https://theawesomedaily.com/wp-content/uploads/2017/09/beach-background-1-1.jpeg"></img>
+        </div>
+        <div>
+          ava + discr
+          <img src=""></img>
+        </div>
+
+        <div>
+          My Posts
+          <div>New post</div>
+          <div>
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
